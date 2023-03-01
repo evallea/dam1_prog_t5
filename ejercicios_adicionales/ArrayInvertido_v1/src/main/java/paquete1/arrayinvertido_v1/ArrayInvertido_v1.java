e004041c65f44719be67af14ff29867ed4969e81
Enrique Valle <enriquevallealcega@gmail.com>
2023-03-01 17:57:48 +0100

    Mejoras en el ejercicio ArrayInvertido_v1

--- ejercicios_adicionales/ArrayInvertido_v1/src/main/java/paquete1/arrayinvertido_v1/ArrayInvertido_v1.java
+++ ejercicios_adicionales/ArrayInvertido_v1/src/main/java/paquete1/arrayinvertido_v1/ArrayInvertido_v1.java
@@ -10,19 +10,21 @@ public class ArrayInvertido_v1 {
 
     public static void main(String[] args) {
 
-        int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int ordenado[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Array sin invertir:");
-        System.out.println(Arrays.toString(numeros));
+        System.out.println(Arrays.toString(ordenado));
 
-        int numerosInver[] = new int[10];
+        int invertido[] = new int[10];
+        // int invertido[] = new int[ordenado.length];
 
-        for (int i = 0; i < numeros.length; i++) {
-            numerosInver[9 - i] = numeros[i];
+        for (int i = 0; i < ordenado.length; i++) {
+            invertido[9 - i] = ordenado[i];
+            // invertido[i]=ordenado[(ordenado.length-1)-i];
         }
 
         System.out.println("Array invertido:");
-        System.out.println(Arrays.toString(numerosInver));
+        System.out.println(Arrays.toString(invertido));
 
     }
 
